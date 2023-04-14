6b01c047414d51c92f63359af5d4ce863f943d3e
Peter Heilbo Ratgen <peter@pratgen.dk>
2023-04-14 13:10:50 +0200

    fix(module-info): moved to a separate file

---
+++ module-info.java
@@ -0,0 +1,16 @@
+module com.badlogic.gdx {
+  requires java.desktop;
+  requires jdk.unsupported;
+  requires gdx.jnigen.loader;
+  exports com.badlogic.gdx.graphics;
+  exports com.badlogic.gdx.graphics.g2d;
+  exports com.badlogic.gdx.backends.lwjgl3;
+  exports com.badlogic.gdx.graphics.glutils;
+  exports com.badlogic.gdx.utils;
+  exports com.badlogic.gdx.utils.viewport;
+  exports com.badlogic.gdx.assets;
+  exports com.badlogic.gdx.assets.loaders;
+  exports com.badlogic.gdx.maps;
+  exports com.badlogic.gdx.maps.tiled;
+  exports com.badlogic.gdx.scenes.scene2d;
+}
