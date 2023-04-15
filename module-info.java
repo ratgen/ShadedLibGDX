27988971829ed430aac3818cd8ac905ceebeae51
Peter Heilbo Ratgen <peter@pratgen.dk>
2023-04-15 13:41:40 +0200

    feat(versioning & packages): automatisation
     - Add automatic versioning based on semver, through Github actions
     - Use the versions plugin to increment
     - Deploy the package built to github packages

---
+++ module-info.java
@@ -0,0 +1,18 @@
+module com.badlogic.gdx {
+  requires java.desktop;
+  requires jdk.unsupported;
+  exports com.badlogic.gdx;
+  exports com.badlogic.gdx.graphics;
+  exports com.badlogic.gdx.graphics.g2d;
+  exports com.badlogic.gdx.backends.lwjgl3;
+  exports com.badlogic.gdx.graphics.glutils;
+  exports com.badlogic.gdx.utils;
+  exports com.badlogic.gdx.utils.viewport;
+  exports com.badlogic.gdx.assets;
+  exports com.badlogic.gdx.assets.loaders;
+  exports com.badlogic.gdx.assets.loaders.resolvers;
+  exports com.badlogic.gdx.maps;
+  exports com.badlogic.gdx.maps.tiled;
+  exports com.badlogic.gdx.maps.tiled.tiles;
+  exports com.badlogic.gdx.scenes.scene2d;
+}
